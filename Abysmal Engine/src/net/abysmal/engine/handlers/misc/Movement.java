d05d7fb74a4e8b96aa147eb4b221d1759f8d4594
Mojken <mikaelgillis@gmail.com>
2015-12-15 22:16:22 +0100

    Update Movement.java

--- Abysmal Engine/src/net/abysmal/engine/handlers/misc/Movement.java
+++ Abysmal Engine/src/net/abysmal/engine/handlers/misc/Movement.java
@@ -38,23 +38,23 @@ public class Movement {
 	}
 
 	void directionalMovement2(int[] keys, Player player) {
-
+		walkToVector(new Vector2(keys[0], 0), player);
 	}
 
-	void directionalMovement3(int[] keys, Player player, int clock) {
-		walkToVector(new Vector2(keys[0], keys[1]), player, clock);
+	void directionalMovement3(int[] keys, Player player) {
+		walkToVector(new Vector2(keys[0], keys[1]), player);
 	}
 
-	void rotationalMovement(int[] keys, Player player, int rotation, int clock) {
-		walkToVectorWithRotation(new Vector2(keys[0], keys[1]), player, rotation, clock);
+	void rotationalMovement(int[] keys, Player player, int rotation) {
+		walkToVectorWithRotation(new Vector2(keys[0], keys[1]), player, rotation);
 	}
 
-	public static void walkToVector(Vector2 vector, Entity entity, int clock) {
-		walkToVectorWithRotation(vector, entity, 0, clock);
+	public static void walkToVector(Vector2 vector, Entity entity) {
+		walkToVectorWithRotation(vector, entity, 0);
 	}
 
-	public static void walkToVectorWithRotation(Vector2 vector, Entity entity, int rotation, int clock) {
-		if (vector.checkProximity(entity.pos) < 0 ) {
+	public static void walkToVectorWithRotation(Vector2 vector, Entity entity, int rotation) {
+		if (vector.checkProximity(entity.pos) < 0) {
 			entity.moving = false;
 		} else {
 			entity.moving = true;
@@ -62,26 +62,16 @@ public class Movement {
 		vector = vector.sub(entity.pos);
 		double phi = java.lang.Math.atan(vector.getX() / vector.getY()) + rotation;
 		if (vector.getY() != java.lang.Math.abs(vector.getY())) phi += Math.TAU / 2;
-//		System.out.println(calculateMomentum(entity));
-		System.out.println( calculateMomentum(entity));
-//		if ((int) (clock % (calculateMomentum(entity) / entity.getMovementSpeed() * 3)) == 0) {
-//			entity.pos.x += (calculateMomentum(entity) / entity.getMovementSpeed()) * entity.stepLength * java.lang.Math.sin(phi % Math.TAU);
-//			entity.pos.y += (calculateMomentum(entity) / entity.getMovementSpeed()) * entity.stepLength * java.lang.Math.cos(phi % Math.TAU);
-//		}
 		entity.pos.x += calculateMomentum(entity) * java.lang.Math.cos(phi % Math.TAU);
 		entity.pos.y += calculateMomentum(entity) * java.lang.Math.sin(phi % Math.TAU);
 	}
 
 	static float calculateMomentum(Entity entity) {
 		if (entity.moving) {
-			if (entity.getMomentum() < entity.getMovementSpeed() && entity.getMomentum() >= 0) {
-				entity.setMomentum(entity.getMomentum() + entity.getAcceleration());
-			}
+			if (entity.getMomentum() < entity.getMovementSpeed()) entity.setMomentum(entity.getMomentum() + entity.getAcceleration());
 			if (entity.getMomentum() < 0) entity.setMomentum(0);
-		}else{
-			if (entity.getMomentum() > 0) {
-				entity.setMomentum(entity.getMomentum() + entity.getAcceleration());
-			}
+		} else {
+			if (entity.getMomentum() > 0) entity.setMomentum(entity.getMomentum() + entity.getAcceleration());
 			if (entity.getMomentum() < 0) entity.setMomentum(0);
 		}
 		return entity.getMomentum();
