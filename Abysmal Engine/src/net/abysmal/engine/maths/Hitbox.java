ae090d55713eb8ec5721ac2cfa2f643958204d55
Mojken Moremeril <Mikaelgillis@gmail.com>
2015-12-16 20:25:34 +0100

    stupid eGit

--- Abysmal Engine/src/net/abysmal/engine/maths/Hitbox.java
+++ Abysmal Engine/src/net/abysmal/engine/maths/Hitbox.java
@@ -11,7 +11,7 @@ public class Hitbox {
 		hitboxVector[1] = new Vector3(entity.getX() + entity.getHitboxPoints()[1].getX(), entity.getY() + entity.getHitboxPoints()[1].getY(), entity.getZ() + entity.getHitboxPoints()[1].getZ());
 	}
 
-	public boolean detectCollition(Hitbox hitbox) {
+	public boolean detectCollision(Hitbox hitbox) {
 		if (hitbox.hitboxVector[0].getX() > hitboxVector[0].getX() && hitbox.hitboxVector[0].getX() < hitboxVector[1].getX() || hitbox.hitboxVector[1].getX() > hitboxVector[0].getX() && hitbox.hitboxVector[1].getX() < hitboxVector[1].getX()) {
 			if (hitbox.hitboxVector[0].getY() > hitboxVector[0].getY() && hitbox.hitboxVector[0].getX() < hitboxVector[1].getY() || hitbox.hitboxVector[1].getY() > hitboxVector[0].getY() && hitbox.hitboxVector[1].getY() < hitboxVector[1].getY()) {
 				if (hitbox.hitboxVector[0].getZ() > hitboxVector[0].getZ() && hitbox.hitboxVector[0].getZ() < hitboxVector[1].getZ() || hitbox.hitboxVector[1].getZ() > hitboxVector[0].getZ() && hitbox.hitboxVector[1].getZ() < hitboxVector[1].getZ()) { return true; }
