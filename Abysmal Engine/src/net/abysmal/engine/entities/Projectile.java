143dfe6633393284e31cceda9fb61cbb4a3c6a3f
Mojken <mikaelgillis@gmail.com>
2015-12-16 15:25:30 +0100

    added projectiles

---
+++ Abysmal Engine/src/net/abysmal/engine/entities/Projectile.java
@@ -0,0 +1,15 @@
+package net.abysmal.engine.entities;
+
+import net.abysmal.engine.maths.Hitbox;
+import net.abysmal.engine.maths.Vector3;
+
+public class Projectile extends Entity {
+	
+	int damage, travelSpeed, statusDuration;
+	Vector3 pos;
+	Hitbox hitbox;
+	
+	public Projectile(Projectile projectile) {
+		hitbox = new Hitbox(projectile);
+	}
+}
