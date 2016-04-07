eb357cd01439d34cbaf563af4308ee7e0e15c867
Mojken Moremeril <Mikaelgillis@gmail.com>
2016-04-07 19:47:40 +0200

    Testing easier window creation class.

--- Abysmal Engine/src/net/abysmal/engine/graphics/Window.java
+++ Abysmal Engine/src/net/abysmal/engine/graphics/Window.java
@@ -14,12 +14,18 @@ public class Window {
 
 	JFrame frame;
 	boolean running = false;
-	
+
 	public Mouse mouseListener;
 	public Keyboard keyboardListener;
-	
+
+	public static int width;
+	public static int height;
+	public static Dimension dimension;
+	public static String title;
+	public static Window w;
+
 	Panel panel;
-	
+
 	public JFrame createWindow(String title, Dimension size, Tick t) {
 		frame = new JFrame(title);
 		panel = new Panel(t);
@@ -29,38 +35,48 @@ public class Window {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setContentPane(panel);
 		frame.setVisible(true);
-		
+
 		mouseListener = new Mouse(frame);
 		keyboardListener = new Keyboard();
-		
+
 		frame.addMouseListener(mouseListener);
 		frame.addKeyListener(keyboardListener);
-		
+
 		running = true;
 		update(t);
 		render(t);
 		return frame;
 	}
-	
+
+	public Window createWindow(String title, int width, Tick t) {
+		Window.width = width;
+		Window.title = title;
+		height = width / 16 * 9;
+		dimension = new Dimension(width, height);
+		createWindow(title, dimension, t);
+		return w;
+	}
+
 	@SuppressWarnings("serial")
-	class Panel extends JPanel{
-		
+	class Panel extends JPanel {
+
 		Tick t;
-		
-		Panel(Tick t){
+
+		Panel(Tick t) {
 			this.t = t;
 		}
-		
+
 		@Override
 		public void paint(Graphics g) {
-			t.render((Graphics2D)g.create());
+			t.render((Graphics2D) g.create());
 		}
 	}
-	
-	private void render(Tick t){
-		new Thread(new Runnable(){
-			public void run(){
-				while (running){
+
+	private void render(Tick t) {
+		new Thread(new Runnable() {
+
+			public void run() {
+				while (running) {
 					panel.repaint();
 					try {
 						Thread.sleep(1000 / FundamentalGameSpecifics.TPS);
@@ -71,11 +87,12 @@ public class Window {
 			}
 		}).start();
 	}
-	
-	private void update(Tick t){
-		new Thread(new Runnable(){
-			public void run(){
-				while (running){
+
+	private void update(Tick t) {
+		new Thread(new Runnable() {
+
+			public void run() {
+				while (running) {
 					t.update();
 					try {
 						Thread.sleep(1000 / FundamentalGameSpecifics.TPS);
@@ -86,5 +103,5 @@ public class Window {
 			}
 		}).start();
 	}
-	
+
 }
