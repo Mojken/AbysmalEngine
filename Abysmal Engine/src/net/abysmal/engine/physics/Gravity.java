9f4591146ea31ff0ec33f11eca0aae9a69636068
Deepstream <william.deepstream@gmail.com>
2016-05-22 23:09:59 +0200

    Optimized collision detection.

--- Abysmal Engine/src/net/abysmal/engine/physics/Gravity.java
+++ Abysmal Engine/src/net/abysmal/engine/physics/Gravity.java
@@ -37,7 +37,33 @@ public class Gravity {
 
 	public static void fall(Entity e, World w) {
 		int tX, tY;
-		for (int i = 0; i < w.tiles[1].length; i++) {
+//		for (int i = 0; i < w.tiles[1].length; i++) {
+//			if (!w.tiles[1][i].solid) continue;
+//			tX = (i % w.mapSize.getWidth()) * w.tileSize;
+//			tY = (i / w.mapSize.getWidth()) * w.tileSize;
+//			e.onGround = false;
+//			if (e.getX() + e.hitboxPoints[1].x > tX && e.getX() + e.hitboxPoints[0].x < tX + w.tileSize) {
+//				if (e.getY() + e.hitboxPoints[1].y >= tY && e.getY() + e.hitboxPoints[0].y < tY + w.tileSize) {
+//					e.onGround = true;
+//					if (e.getY() + e.hitboxPoints[1].y > tY && e.getY() + e.hitboxPoints[0].y < tY + w.tileSize) e.teleport(new Vector(e.getX(), tY - e.hitboxPoints[1].y));
+//					break;
+//				}
+//			}
+//		}
+		
+		int[] indexes = {
+				(int)(e.getX()/w.tileSize) + (w.mapSize.getWidth() * (int)(e.getY() / w.tileSize)),
+				(int)(e.getX()/w.tileSize + (int)(e.getX()/w.tileSize) == w.mapSize.getWidth()-1 ? 0 : 1) + (w.mapSize.getWidth() * (int)(e.getY() / w.tileSize)),
+				(int)(e.getX()/w.tileSize - (int)(e.getX()/w.tileSize) == 0 ? 0 : 1) + (w.mapSize.getWidth() * (int)(e.getY() / w.tileSize)),
+				(int)(e.getX()/w.tileSize) + (w.mapSize.getWidth() * ((int)(e.getY() / w.tileSize) + ((int)(e.getY() / w.tileSize) == w.mapSize.getHeight()-1 ? 0 : 1))),
+				(int)(e.getX()/w.tileSize + (int)(e.getX()/w.tileSize) == w.mapSize.getWidth()-1 ? 0 : 1) + (w.mapSize.getWidth() * ((int)(e.getY() / w.tileSize) + ((int)(e.getY() / w.tileSize) == w.mapSize.getHeight()-1 ? 0 : 1))),
+				(int)(e.getX()/w.tileSize - (int)(e.getX()/w.tileSize) == 0 ? 0 : 1) + (w.mapSize.getWidth() * ((int)(e.getY() / w.tileSize) + ((int)(e.getY() / w.tileSize) == w.mapSize.getHeight()-1 ? 0 : 1))),
+				(int)(e.getX()/w.tileSize) + (w.mapSize.getWidth() * ((int)(e.getY() / w.tileSize) - ((int)(e.getY() / w.tileSize) == 0 ? 0 : 1))),
+				(int)(e.getX()/w.tileSize + (int)(e.getX()/w.tileSize) == w.mapSize.getWidth()-1 ? 0 : 1) + (w.mapSize.getWidth() * ((int)(e.getY() / w.tileSize) - ((int)(e.getY() / w.tileSize) == 0 ? 0 : 1))),
+				(int)(e.getX()/w.tileSize - (int)(e.getX()/w.tileSize) == 0 ? 0 : 1) + (w.mapSize.getWidth() * ((int)(e.getY() / w.tileSize) - ((int)(e.getY() / w.tileSize) == 0 ? 0 : 1)))
+		};
+		
+		for (int i : indexes) {
 			if (!w.tiles[1][i].solid) continue;
 			tX = (i % w.mapSize.getWidth()) * w.tileSize;
 			tY = (i / w.mapSize.getWidth()) * w.tileSize;
